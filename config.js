4207aeaf8a65f8e8d7e3f515c526028e1e8628f9
telegram256 <nasirhussain244pan24@gmail.com>
2025-03-28 22:23:21 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "NONQGQRQ#roypOBrpWicHpJ8XS4VhfzdDPfI5u-MCw7guKREjQhM",
     CAPTION: process.env.CAPTION || "POWERED BY SOBIA BUTT",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
